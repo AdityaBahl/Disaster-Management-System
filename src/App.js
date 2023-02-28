6a0ab777d7f0695d9f586e9ae48261afb66f6a67
Aditya Bahl <adityabahl12345@gmail.com>
2023-02-28 11:13:01 +0530

    Material Tailwind Compatiblity Added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar'
 function App() {
   return (
-    <><Navbar /><div>
-      <form>
-        <input type="text" placeholder="Full name" />
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <button>Sign In</button>
-      </form>
-    </div></>
+    <Navbar />
   );
 }
 
