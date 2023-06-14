badf21e2c5e52dd77b61a6232aee46c111320fff
Aditya Bahl <adityabahl12345@gmail.com>
2023-06-14 23:13:35 +0530

    Legacy Files before ML component Added

---
+++ Legacy/BeforeMLComponentAdded/Nearbyhelp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar';
+const Nearbyhelp = () => {
+  return (
+    <div className='container flex flex-col item-center justify-center'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Nearbyhelp
